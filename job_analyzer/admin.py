44845ca14caae8bb64475bbe7347b9b00e8ac220
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-07 22:29:15 +0100

    feat(admin): register JobPosting with filters/search

--- job_analyzer/admin.py
+++ job_analyzer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import JobPosting
 
 # Register your models here.
+@admin.register(JobPosting)
+class JobPostingAdmin(admin.ModelAdmin):
+    list_display = ("id", "target_field", "match_score", "created_at")
+    list_filter = ("target_field",)
+    search_fields = ("raw_text",)
+    readonly_fields = ("created_at", "updated_at")
