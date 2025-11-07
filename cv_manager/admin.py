f5a6e46dc612ad7cb8eb5d4311f71fd254956328
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-07 21:51:18 +0100

    feat(admin): register Education with list/search

--- cv_manager/admin.py
+++ cv_manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CV
+from .models import CV, Education
 
 # Register your models here.
 
@@ -8,3 +8,10 @@ class CVAdmin(admin.ModelAdmin):
     list_display = ("full_name", "field", "is_primary", "email", "updated_at")
     list_filter = ("field", "is_primary")
     search_fields = ("full_name", "email", "address", "github")
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ("degree", "institution", "cv", "start_date", "end_date", "status")
+    list_filter = ("status", "institution")
+    search_fields = ("degree", "institution", "cv__full_name")
+    list_select_related = ("cv",)
