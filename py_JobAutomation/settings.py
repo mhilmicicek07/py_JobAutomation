935778ba7c72eec84da80d06665974446910de22
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-07 22:27:01 +0100

    chore(settings): register job_analyzer

--- py_JobAutomation/settings.py
+++ py_JobAutomation/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 INSTALLED_APPS = [
     'cv_manager',
+    'job_analyzer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
