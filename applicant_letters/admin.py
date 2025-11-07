0e08b7d02d039acf25fff41455da2d9fed15c877
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-07 23:51:44 +0100

    feat(admin): register ApplicationDraft

--- applicant_letters/admin.py
+++ applicant_letters/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ApplicationDraft
 
 # Register your models here.
+@admin.register(ApplicationDraft)
+class ApplicationDraftAdmin(admin.ModelAdmin):
+    list_display = ("id", "posting", "cv", "language", "created_at")
+    search_fields = ("cover_letter",)
+    readonly_fields = ("created_at", "updated_at")
