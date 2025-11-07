f7e10526a62d7c75897fa8f8a45e3f832d8dd505
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-07 21:38:28 +0100

    feat(admin): register CV with filters and search

--- cv_manager/admin.py
+++ cv_manager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CV
 
 # Register your models here.
+
+@admin.register(CV)
+class CVAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "field", "is_primary", "email", "updated_at")
+    list_filter = ("field", "is_primary")
+    search_fields = ("full_name", "email", "address", "github")
