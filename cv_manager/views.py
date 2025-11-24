0f7898dcb03430cf88a032dc05834aac7a8f4898
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-24 23:24:22 +0100

    Add basic CV dashboard view

--- cv_manager/views.py
+++ cv_manager/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import CV
 
 # Create your views here.
+def dashboard(request):
+    """
+    Basit CV listesi view'i.
+    Şimdilik sadece tüm CV'leri context'e verir.
+    Template ve URL yönlendirmesini sonraki adımlarda ekleyeceğiz.
+    """
+    cvs = CV.objects.all().order_by("full_name", "field")
+    context = {
+        "cvs": cvs,
+    }
+    return render(request, "cv_manager/dashboard.html", context)
