50f84d07941b839566429be70ff71695058cb5bc
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-24 23:52:13 +0100

    Wire cv_manager URLconf into project URLs

--- py_JobAutomation/urls.py
+++ py_JobAutomation/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from cv_manager.views import dashboard
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", dashboard, name="dashboard"),
+    path("", include("cv_manager.urls")),
 ]
