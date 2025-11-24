b6f13f413d7c81309ecef8f953a4d1e8501b314c
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-24 23:29:23 +0100

    Route project homepage to CV dashboard

--- py_JobAutomation/urls.py
+++ py_JobAutomation/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cv_manager.views import dashboard
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", dashboard, name="dashboard"),
 ]
