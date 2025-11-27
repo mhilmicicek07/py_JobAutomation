fb2eb3db48ec26f8218d510e7632cc63350e1ff7
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-11-27 22:46:27 +0100

    Add URL pattern for editing existing CVs

--- cv_manager/urls.py
+++ cv_manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import dashboard, cv_create
+from .views import *
 
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
     path("cv/new/", cv_create, name="cv_create"),
+    path("cv/<int:pk>/edit/", cv_update, name="cv_update"),
 ]
