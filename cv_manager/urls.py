35e49969a8bb43e585805da9307c7a7dcef64258
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-12-06 20:53:31 +0100

    Add URL route for CV deletion

--- cv_manager/urls.py
+++ cv_manager/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("cv/new/", cv_create, name="cv_create"),
     path("cv/<int:pk>/edit/", cv_update, name="cv_update"),
     path("cv/<int:pk>/import/", cv_import, name="cv_import"),
+    path("cv/<int:pk>/delete/", cv_delete, name="cv_delete"),
 ]
